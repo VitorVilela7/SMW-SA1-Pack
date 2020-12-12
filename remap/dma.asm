c14d079bfbacc6524fadcbdbab032bd639d94b6b
Vitor Vilela <vitor.vilela.mario@gmail.com>
2020-12-12 18:46:02 -0300

    Only remap required channels

--- remap/dma.asm
+++ remap/dma.asm
@@ -70,212 +70,7 @@ STA $4325
 org $008463
 LDY #$04               
 
-org $00923D
-STA $4320,x
-
-org $009243
-LDA #$04
-
-org $0098C0
-STA $4320
-
-org $0098CD
-STA $4322
-
-org $0098D2
-STA $4324
-
-org $0098D8
-STA $4325
-
-org $0098DB
-LDY #$04
-
-org $0098FC
-STA $4320
-
-org $009901
-STA $4324
-
-org $00990E
-STA $4322
-
-org $009918
-STA $4325
-
-org $009904
-LDX #$04
-
-org $00A311
-STA $4320
-
-org $00A317
-STA $4322
-
-org $00A31C
-STY $4324
-
-org $00A322
-STA $4325
-
-org $00A302
-LDX #$04
-
-org $00A330
-STA $4320
-
-org $00A33C
-STA $4322
-
-org $00A341
-STY $4324
-
-org $00A347
-STA $4325
-
-org $00A358
-STA $4322               
-
-org $00A35E
-STA $4325               
-
-org $00A361
-LDY #$04                
-
-org $00A378
-STA $4322               
-
-org $00A37E
-STA $4325               
-
-org $00A381
-LDY #$04                
-
-org $00A39A
-STA $4320               
-
-org $00A39F
-STY $4324               
-
-org $00A3A2
-LDX #$04                
-
-org $00A3AF
-STA $4322               
-
-org $00A3B5
-STA $4325               
-
-org $00A3C6
-STA $4322               
-
-org $00A3CC
-STA $4325               
-
-org $00A3E2
-STA $4322               
-
-org $00A3E8
-STA $4325               
-
-org $00A3F3
-STA $4322               
-
-org $00A3F9
-STA $4325               
-
-org $00A40C
-STA $4322               
-
-org $00A412
-STA $4325               
-
-org $00A44E
-STA $4320               
-
-org $00A454
-STA $4322               
-
-org $00A459
-STY $4324               
-
-org $00A45F
-STA $4325               
-
-org $00A462
-LDX #$04                
-
-org $00A470
-STA $4322               
-
-org $00A476
-STA $4325               
-
-org $00A4A4
-STX $4324               
-
-org $00A4A7
-STA $4325               
-
-org $00A4AC
-STZ $4326               
-
-org $00A4BA
-STA $4320               
-
-org $00A4BF
-STA $4322               
-
-org $00A4C8
-LDA #$04                
-
-org $00A4F3
-STY $4320               
-
-org $00A4F9
-STY $4322               
-
-org $00A4FC
-STZ $4324               
-
-org $00A502
-STY $4325               
-
-org $00A505
-LDA #$04               
-
-org $00A7D2
-STA $4320               
-
-org $00A7D8
-STA $4322               
-
-org $00A7DD
-STX $4324               
-
-org $00A7E3
-STA $4325               
-
-org $00A7E6
-LDX #$04                
-
-org $00A7F4
-STA $4322               
-
-org $00A7FA
-STA $4325               
-
-org $00A809
-STA $4322               
-
-org $00A80F
-STA $4325               
-
-org $00A81E
-STA $4322               
-
-org $00A824
-STA $4325               
+;----
 
 org $008611
 STA $4320,x
@@ -288,6 +83,8 @@ STA $4320,x
 
 org $00863A
 STA $4321
+
+;----
 
 org $00874E
 STA $4320               
@@ -325,6 +122,11 @@ STA $4320,X
 org $0087FE
 LDA #$04
 
+org $008720
+STA $4324               
+
+org $00873E
+STA $4321
 
 org $008818
 STA $4320,X 
@@ -363,7 +165,6 @@ STA $4320,X
 org $0088AE
 LDA #$04 
 
-
 org $0088CD
 STA $4320,X 
 
@@ -373,20 +174,17 @@ STA $4325
 org $0088D8
 LDA #$04                
 
-
 org $008909
 STA $4320,X 
 
 org $00890F
 LDA #$04                
 
-
 org $00892B
 STA $4320,X 
 
 org $008931
-LDA #$04 
-
+LDA #$04
 
 org $00894B
 STA $4320,X  
@@ -394,20 +192,17 @@ STA $4320,X
 org $008951
 LDA #$04
 
-
 org $00896E
 STA $4320,X  
 
 org $008974
 LDA #$04
 
-
 org $008992
 STA $4320,X 
 
 org $008998
 LDA #$04
-
 
 org $0089B4
 STA $4320,X 
@@ -416,15 +211,13 @@ org $0089BC
 STA $4325               
 
 org $0089BF
-LDA #$04    
-
+LDA #$04
 
 org $0089DB
 STA $4320,X  
 
 org $0089E1
 LDA #$04
-
 
 org $008A00
 STA $4320,X 
@@ -433,8 +226,9 @@ org $008A08
 STA $4325               
 
 org $008A0B
-LDA #$04 
+LDA #$04
 
+;----
 
 org $008D13
 STA $4320,X             
@@ -448,7 +242,6 @@ STA $4320,X
 
 org $008D38
 LDA #$04
-
 
 org $008D51
 STA $4320,X             
@@ -470,13 +263,13 @@ STA $4320,X
 org $008DC4
 LDA #$04                
 
-
 org $008DDB
 STA $4320,X             
 
 org $008DE1
 LDA #$04                
 
+;----
 
 org $00A53F
 STA $4320,X  
@@ -493,13 +286,13 @@ STA $4323
 org $00A55F
 LDA #$04  
 
-
 org $00A57A
 STA $4320,X 
 
 org $00A580
-LDA #$04  
+LDA #$04
 
+;----
 
 org $04D754
 STA $4320,x
@@ -507,10 +300,8 @@ STA $4320,x
 org $04D76A
 LDA #$04 
 
-
 org $0C9580
-STA $4320,Y   
-
+STA $4320,Y
 
 org $0C958C
 ORA $4323               
@@ -532,12 +323,6 @@ STA $4323
 
 org $0C95BD
 LDA #$04
-
-org $008720
-STA $4324               
-
-org $00873E
-STA $4321
 
 org $04D767
 STA $4323               
